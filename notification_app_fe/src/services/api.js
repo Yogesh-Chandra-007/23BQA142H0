43816ee09232634d5e56409bbbaf774b8d2de22c
Yogesh-Chandra-007 <talluriyogeshchandra@gmail.com>
2026-06-05 12:02:25 +0530

    Update api.js

--- notification_app_fe/src/services/api.js
+++ notification_app_fe/src/services/api.js
@@ -1,1 +1,19 @@
+const API_URL =
+  "http://4.224.186.213/evaluation-service/notifications";
 
+export async function fetchNotifications(token) {
+
+  const response = await fetch(API_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch notifications");
+  }
+
+  const data = await response.json();
+
+  return data.notifications;
+}
