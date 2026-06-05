615d794627302cbc59966f5f804654898aa58427
Yogesh-Chandra-007 <talluriyogeshchandra@gmail.com>
2026-06-05 11:10:22 +0530

    Add Notification class with properties and methods

--- notification_app_be/Notification.java
+++ notification_app_be/Notification.java
@@ -1,1 +1,39 @@
+public class Notification {
 
+    private String id;
+    private String type;
+    private long timestamp;
+    private boolean read;
+
+    public Notification(String id, String type, long timestamp, boolean read) {
+        this.id = id;
+        this.type = type;
+        this.timestamp = timestamp;
+        this.read = read;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
