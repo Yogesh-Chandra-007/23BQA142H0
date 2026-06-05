482d43b885434ccc06b4154d1bfed33656c328e8
Yogesh-Chandra-007 <talluriyogeshchandra@gmail.com>
2026-06-05 12:06:58 +0530

    Update NotificationCard.jsx

--- notification_app_fe/src/components/NotificationCard.jsx
+++ notification_app_fe/src/components/NotificationCard.jsx
@@ -1,1 +1,9 @@
-
+export default function NotificationCard({ notification }) {
+  return (
+    <div>
+      <h3>{notification.Type}</h3>
+      <p>{notification.Message}</p>
+      <small>{notification.Timestamp}</small>
+    </div>
+  );
+}
